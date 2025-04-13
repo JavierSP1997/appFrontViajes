752af093743c7b5e3a5c0afa750c1c83b13b2c71
Laura Moreno <chipifsdev@gmail.com>
2025-04-13 21:44:14 +0200

    Nav básico del perfil de usuario

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
