0057d44151f41409ec191974c991ecd6f611f4b5
Laura Moreno <chipifsdev@gmail.com>
2025-04-13 21:36:50 +0200

    Añadida ruta vacía y errónea redirigida a la home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,11 @@ import { UserProfileComponent } from './pages/user-profile/user-profile.componen
 import { EditUserProfileComponent } from './pages/edit-user-profile/edit-user-profile.component';
 
 export const routes: Routes = [
-    {path: '', pathMatch: 'full', component: HomeComponent},
+    { path: '', pathMatch: 'full', component: HomeComponent },
     { path: "login", component: LoginComponent },
-    {path: 'perfil-usuario', component: UserProfileComponent,
+    { path: 'perfil-usuario', component: UserProfileComponent,
         children: [
-            {path: 'editar', component: EditUserProfileComponent}] },
-    {path: "**", redirectTo: "login"}
+            { path: 'editar', component: EditUserProfileComponent }] 
+    },
+    { path: "**", component: HomeComponent },
 ];
