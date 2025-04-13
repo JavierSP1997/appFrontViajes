27ec65ab8ff3ebdf500065d8264810d32b8f625a
Laura Moreno <chipifsdev@gmail.com>
2025-04-13 20:29:53 +0200

    Creado los componentes home, userProfile y editUserProfile y añadido al archivo de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { EditUserProfileComponent } from './pages/edit-user-profile/edit-user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', pathMatch: 'full', component: HomeComponent},
+    {path: 'perfil-usuario', component: UserProfileComponent,
+        children: [
+            {path: 'editar', component: EditUserProfileComponent}] }
+];
