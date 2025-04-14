40c68d2d0a55a03f8cd55dcb99f645b70c75daae
Cora <enero.noventaytres@gmail.com>
2025-04-14 11:54:19 +0200

    creacion de componetente guia | estilado con colores de las variables |

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { GuideComponent } from "../../conponents/guide/guide.component";
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, GuideComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
