1ec0b144bc4da488de84138ce1d46a9bbcd0c382
Laura Moreno <chipifsdev@gmail.com>
2025-04-15 00:13:57 +0200

    Resolviendo conflictos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import type { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
-import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 import { EditUserProfileComponent } from './pages/edit-user-profile/edit-user-profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { MyProfileComponent } from './pages/my-profile/my-profile.component';
@@ -14,7 +13,7 @@ export const routes: Routes = [
   { path: 'viajes', component: ViajesComponent },
   {
     path: 'perfil-usuario',
-    component: UserProfileComponent,
+    component: MyProfileComponent,
     children: [
       { path: '', component: MyProfileComponent },
       { path: 'editar', component: EditUserProfileComponent },
