4073143e7a9446e8da93199485c7913b8d4e249c
Cora <enero.noventaytres@gmail.com>
2025-04-14 17:41:30 +0200

    comienzo y edicion del profile

--- src/app/services/viajes.service.ts
+++ src/app/services/viajes.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
 @Injectable({
+
   providedIn: 'root'
 })
 export class ViajesService {
@@ -9,7 +10,6 @@ export class ViajesService {
   private httpClient = inject(HttpClient);
   private baseUrl = 'http://localhost:3000/api/viajes';
   //mirar bien lo del enviroment
-  
   getLastTrip() {
     return this.httpClient.get<any>(`${this.baseUrl}`)
   }
