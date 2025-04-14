9e15686139379626faf22aaec6de115e9dca1658
Laura Moreno <chipifsdev@gmail.com>
2025-04-14 16:48:28 +0200

    Añadido el selector del nav a la home e importado en el ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { GuideComponent } from "../../components/guide/guide.component";
 import { LastTripsComponent } from "../../components/last-trips/last-trips.component";
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
 	selector: "app-home",
-	imports: [RouterLink, GuideComponent, LastTripsComponent],
+	imports: [RouterLink, GuideComponent, LastTripsComponent, HeaderComponent],
 	templateUrl: "./home.component.html",
 	styleUrl: "./home.component.css",
 })
