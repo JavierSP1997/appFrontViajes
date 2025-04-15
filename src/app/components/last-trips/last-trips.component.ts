3a8590820722a4b724401e42aaa218e823d9b61a
Laura Moreno <chipifsdev@gmail.com>
2025-04-15 12:58:24 +0200

    terminado el componente para mostrar los ultimos viajes publicados

--- src/app/components/last-trips/last-trips.component.ts
+++ src/app/components/last-trips/last-trips.component.ts
@@ -1,27 +1,25 @@
-import { Component, inject } from '@angular/core';
-import type { Viaje } from '../../../../interfaces/viaje.interface';
-import { ViajesService } from '../../services/viajes.service';
-import { DatePipe } from '@angular/common';
+import { Component, inject } from "@angular/core";
+import type { Viaje } from "../../../../interfaces/viaje.interface";
+import { ViajesService } from "../../services/viajes.service";
+import { DatePipe } from "@angular/common";
 
 @Component({
-  selector: 'app-last-trips',
-  imports: [DatePipe],
-  templateUrl: './last-trips.component.html',
-  styleUrl: './last-trips.component.css',
+	selector: "app-last-trips",
+	imports: [DatePipe],
+	templateUrl: "./last-trips.component.html",
+	styleUrl: "./last-trips.component.css",
 })
 export class LastTripsComponent {
-  
-  viajes: Viaje[] = [];
-  lastThreeTrips: Viaje[] = [];
-  private viajeService = inject(ViajesService);
+	viajes: Viaje[] | undefined = [];
+	private viajeService = inject(ViajesService);
 
-  ngOnInit() {
-    try {
-      this.viajeService.getLastViaje().subscribe((viajes: Viaje[]) => {
-        this.viajes = viajes.reverse().slice(0, 3);
-      });
-    } catch (error) {
-      console.log('error al cargar los viajes', error);
-    }
-  }
+	ngOnInit() {
+		try {
+			this.viajeService.getLastViaje().subscribe((viajes: Viaje[]) => {
+				this.viajes = viajes.reverse().slice(0, 3);
+			});
+		} catch (error) {
+			console.log("error al cargar los viajes", error);
+		}
+	}
 }
