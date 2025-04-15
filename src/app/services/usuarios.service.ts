0bfe338ed1ee729a56bdfab6a5d3369607c3d7b0
Javier Sancho <sanchopej@gmail.com>
2025-04-15 17:57:34 +0200

    Creacion del validator para comprobar email, register ts y html listos para que funcione la comprobacion de duplicado de email

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,27 +1,27 @@
 import { HttpClient } from "@angular/common/http";
 import { inject, Injectable } from "@angular/core";
-import { lastValueFrom } from "rxjs";
+import { lastValueFrom, map } from "rxjs";
 import type { Usuario } from "../../../interfaces/usuario.interface";
 
 type Body = { nombre?: string; email: string; password: string };
 type LoginResponse = { message: string; token: string };
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: "root",
 })
 export class UsuariosService {
-  private httpClient = inject(HttpClient);
-  private baseUrl = 'http://localhost:3000/api/usuarios';
+	private httpClient = inject(HttpClient);
+	private baseUrl = "http://localhost:3000/api/usuarios";
 
-  getAll() {
-    return lastValueFrom(this.httpClient.get<Usuario[]>(`${this.baseUrl}`));
-  }
+	getAll() {
+		return lastValueFrom(this.httpClient.get<Usuario[]>(`${this.baseUrl}`));
+	}
 
-  login(body: Body) {
-    return lastValueFrom(
-      this.httpClient.post<LoginResponse>(`${this.baseUrl}/login`, body)
-    );
-  }
+	login(body: Body) {
+		return lastValueFrom(
+			this.httpClient.post<LoginResponse>(`${this.baseUrl}/login`, body),
+		);
+	}
 
 	register(body: Body) {
 		return lastValueFrom(
@@ -33,14 +33,17 @@ export class UsuariosService {
 		return lastValueFrom(this.httpClient.get<Usuario>(`${this.baseUrl}/${id}`));
 	}
 
-  update(id: number, body: Usuario) {
-    return lastValueFrom(
-      this.httpClient.put<Usuario>(`${this.baseUrl}/${id}`, body)
-    );
-  }
-  
-  }
-  
+	update(id: number, body: Usuario) {
+		return lastValueFrom(
+			this.httpClient.put<Usuario>(`${this.baseUrl}/${id}`, body),
+		);
+	}
 
-
-
+	emailExiste(email: string) {
+		return lastValueFrom(
+			this.httpClient
+				.get<{ existe: boolean }>(`${this.baseUrl}/check-email?email=${email}`)
+				.pipe(map((res) => res.existe)),
+		);
+	}
+}
