6daa7dfdb151681d8695d909361a826a14ae256d
Javier Sancho <sanchopej@gmail.com>
2025-04-15 16:01:49 +0200

    Creacion de tripcomponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,17 +6,18 @@ import { RegisterComponent } from "./pages/register/register.component";
 import { MyProfileComponent } from "./pages/my-profile/my-profile.component";
 import { ViajesComponent } from "./pages/viajes/viajes.component";
 import { authGuard } from "../guards/auth.guard";
+import { TripComponent } from "./pages/trip/trip.component";
 
 export const routes: Routes = [
 	{ path: "", pathMatch: "full", component: HomeComponent },
 	{ path: "register", component: RegisterComponent },
 	{ path: "login", component: LoginComponent },
 	{ path: "viajes", component: ViajesComponent },
-	/* {
+	{
 		path: "viaje/:idViaje",
-		component: ViajeComponent,
+		component: TripComponent,
 		canActivate: [authGuard],
-	}, */
+	},
 	{ path: "perfil-usuario/:id", component: MyProfileComponent },
 	{ path: "perfil-usuario/:id/editar", component: EditUserProfileComponent },
 	{ path: "**", component: HomeComponent },
