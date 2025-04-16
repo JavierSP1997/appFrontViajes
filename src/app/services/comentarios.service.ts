9bf3945d6a43dee52df7daa95027cd7676a81423
Javier Sancho <sanchopej@gmail.com>
2025-04-16 17:07:53 +0200

    Creacion de la interfaz comentario

--- src/app/services/comentarios.service.ts
+++ src/app/services/comentarios.service.ts
@@ -1,6 +1,33 @@
-import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import type { Comentario } from "../../../interfaces/comentario";
+import { lastValueFrom } from "rxjs";
 
 @Injectable({
 	providedIn: "root",
 })
-export class ComentariosService {}
+export class ComentariosService {
+	private httpClient = inject(HttpClient);
+	private baseUrl = "http://localhost:3000/api/viajes";
+
+	obtenerComentarios(viajeId: number) {
+		return lastValueFrom(
+			this.httpClient.get<Comentario[]>(
+				`${this.baseUrl}/${viajeId}/comentarios`,
+			),
+		);
+	}
+
+	crearComentario(
+		viajeId: number,
+		body: {
+			usuarios_id_usuario: number;
+			viajes_id_viaje: number;
+			comentario: string;
+		},
+	) {
+		return lastValueFrom(
+			this.httpClient.post(`${this.baseUrl}/${viajeId}/comentarios`, body),
+		);
+	}
+}
