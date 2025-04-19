aee026ede7623c4f8fa2293751890a9c04e50d94
Javier Sancho <sanchopej@gmail.com>
2025-04-19 23:30:26 +0200

    Ya se muestran las reseñas con su autor, falta put y delete y la puntuacion con estrellas

---
+++ src/app/components/review/review.component.ts
@@ -0,0 +1,80 @@
+import { Component, Input, type OnInit, inject } from "@angular/core";
+import type { Review } from "../../../../interfaces/review.interface";
+import { FormsModule } from "@angular/forms";
+import { ReviewService } from "../../services/review.service";
+import { UsuariosService } from "../../services/usuarios.service";
+import { DatePipe } from "@angular/common";
+
+@Component({
+	selector: "app-review",
+	imports: [FormsModule, DatePipe],
+	templateUrl: "./review.component.html",
+	styleUrls: ["./review.component.css"],
+})
+export class ReviewsComponent implements OnInit {
+	@Input() viajeId!: number;
+
+	reviews: Review[] = [];
+	nuevaReview = "";
+	puntuacion = 5;
+	private reviewService = inject(ReviewService);
+	private usuariosService = inject(UsuariosService);
+	token: string = localStorage.getItem("token") || "";
+	usuarioId: number | null = null;
+
+	async ngOnInit() {
+		if (this.viajeId) {
+			const usuario = await this.usuariosService.getPerfilUsuario();
+			this.usuarioId = usuario.id_usuario;
+			await this.cargarReviews();
+		}
+	}
+
+	async cargarReviews() {
+		try {
+			const reviews = await this.reviewService.getByViajeId(this.viajeId);
+			console.log("📦 Reviews cargadas:", reviews);
+			this.reviews = reviews.sort(
+				(a, b) => new Date(a.fecha).getTime() - new Date(b.fecha).getTime(),
+			);
+		} catch (error) {
+			console.error("Error cargando las reseñas", error);
+		}
+	}
+
+	async enviarReview() {
+		if (!this.nuevaReview.trim()) return;
+
+		try {
+			if (this.usuarioId) {
+				await this.reviewService.createReview({
+					id_usuario: this.usuarioId,
+					viajes_id_viaje: this.viajeId,
+					puntuacion: this.puntuacion,
+					review: this.nuevaReview,
+					fecha: new Date().toISOString().split("T")[0],
+					id_review: 0,
+					nombre_usuario: "",
+				});
+				this.nuevaReview = "";
+				await this.cargarReviews();
+			}
+		} catch (error) {
+			console.error("Error al enviar la reseña", error);
+		}
+	}
+
+	async eliminarReview(id: number) {
+		try {
+			const review = this.reviews.find((r) => r.id_review === id);
+			if (review && review.id_usuario === this.usuarioId) {
+				await this.reviewService.deleteReview(id);
+				await this.cargarReviews();
+			} else {
+				console.error("No tienes permiso para eliminar esta reseña");
+			}
+		} catch (error) {
+			console.error("Error al eliminar la reseña", error);
+		}
+	}
+}
---
+++ src/app/services/review.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { lastValueFrom, map } from "rxjs";
+import type { Review } from "../../../interfaces/review.interface";
+
+@Injectable({
+	providedIn: "root",
+})
+export class ReviewService {
+	private httpClient = inject(HttpClient);
+	private baseUrl = "http://localhost:3000/api/reviews";
+
+	getAll() {
+		return lastValueFrom(this.httpClient.get<Review[]>(`${this.baseUrl}`));
+	}
+
+	getByViajeId(viajeId: number) {
+		return lastValueFrom(
+			this.httpClient.get<Review[]>(`${this.baseUrl}/viaje/${viajeId}/usuario`),
+		);
+	}
+
+	createReview(body: Review) {
+		const token = localStorage.getItem("token");
+		return lastValueFrom(
+			this.httpClient.post<Review>(`${this.baseUrl}`, body, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}),
+		);
+	}
+
+	updateReview(id: number, body: Review) {
+		const token = localStorage.getItem("token");
+		return lastValueFrom(
+			this.httpClient.put<Review>(`${this.baseUrl}/${id}`, body, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}),
+		);
+	}
+
+	deleteReview(id: number) {
+		const token = localStorage.getItem("token");
+		return lastValueFrom(
+			this.httpClient.delete(`${this.baseUrl}/${id}`, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}),
+		);
+	}
+}
