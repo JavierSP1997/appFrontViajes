ce7158ea9fc649df2aa7a62e0e37b0975d2f4c19
Laura Moreno <chipifsdev@gmail.com>
2025-04-21 16:59:14 +0200

    Creada la funcionalidad y la estética de "mis viajes" siguiendo la linea estética de la página

--- src/app/pages/mis-viajes/mis-viajes.component.ts
+++ src/app/pages/mis-viajes/mis-viajes.component.ts
@@ -2,28 +2,33 @@ import { Component, inject } from "@angular/core";
 import { UsuariosService } from "../../services/usuarios.service";
 import { ViajesService } from "../../services/viajes.service";
 import type { Viaje } from "../../../../interfaces/viaje.interface";
-import Swal from "sweetalert2";
+import { DatePipe } from "@angular/common";
+import { Router } from "@angular/router";
 
 @Component({
 	selector: "app-mis-viajes",
-	imports: [],
+	imports: [DatePipe],
 	templateUrl: "./mis-viajes.component.html",
 	styleUrl: "./mis-viajes.component.css",
 })
 export class MisViajesComponent {
 	private usuariosService = inject(UsuariosService);
 	private viajesService = inject(ViajesService);
+	private router = inject(Router);
 
 	viajesDelUsuario: Viaje[] = [];
 
 	async ngOnInit() {
 		try {
 			const usuario = await this.usuariosService.getPerfilUsuario();
-			const idUsuario = usuario.id_usuario;
-			this.viajesDelUsuario =
-				await this.viajesService.getViajesUsuario(idUsuario);
+			const id = usuario.id_usuario;
+			this.viajesDelUsuario = await this.viajesService.getViajesByUsuario(id);
 		} catch (error) {
 			console.log(error);
 		}
+	}
+
+	irATrip(idViaje: number): void {
+		this.router.navigate(["/viajes", idViaje]);
 	}
 }
