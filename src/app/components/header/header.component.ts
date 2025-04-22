afc600a7ccee99387295e969ef5bea509567ebc8
Cora <enero.noventaytres@gmail.com>
2025-04-22 22:32:49 +0200

    ts de si esta logao

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,17 @@
 import { Component, signal } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { NotificacionesComponent } from "../notificaciones/notificaciones.component";
 
 @Component({
 	selector: "app-header",
-	imports: [RouterLink],
+	imports: [RouterLink, NotificacionesComponent],
 	templateUrl: "./header.component.html",
 	styleUrl: "./header.component.css",
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+	isLoggedIn = false;
+
+	ngOnInit(): void {
+	  this.isLoggedIn = !!localStorage.getItem("token"); 
+	}
+}
