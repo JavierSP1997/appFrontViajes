d4a3f0fe0bd5a3f2516f6f12d63b99e4e96708f9
Cora <enero.noventaytres@gmail.com>
2025-04-22 22:47:49 +0200

    no se quitarlo si esta logado

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,17 +1,10 @@
 import { Component, signal } from "@angular/core";
 import { RouterLink } from "@angular/router";
-import { NotificacionesComponent } from "../notificaciones/notificaciones.component";
 
 @Component({
 	selector: "app-header",
-	imports: [RouterLink, NotificacionesComponent],
+	imports: [RouterLink],
 	templateUrl: "./header.component.html",
 	styleUrl: "./header.component.css",
 })
-export class HeaderComponent {
-	isLoggedIn = false;
-
-	ngOnInit(): void {
-	  this.isLoggedIn = !!localStorage.getItem("token"); 
-	}
-}
+export class HeaderComponent {}
