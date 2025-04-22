ce376dd9ce23b749e95d14599ad89addb17cfbaf
Cora <enero.noventaytres@gmail.com>
2025-04-22 22:31:50 +0200

    cambiando interface

--- interfaces/notificacion.interface.ts
+++ interfaces/notificacion.interface.ts
@@ -1,5 +1,9 @@
 export interface Notificacion {
-    id_notificacion: number;
-    mensaje: string;
-    fecha_notificacion: string; 
-  }
+	id_notificacion: number;
+	id_usuario: number;
+	id_viaje: number;
+	mensaje: string;
+	// tipo: string;
+	fecha_notificacion: string;
+	leido: boolean;
+}
